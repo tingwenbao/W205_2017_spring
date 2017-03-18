6194d75e49fcced4b424164406073f4a40462d8c
tingwenbao <tingwenbao@berkeley.edu>
2017-03-18 22:25:10 +0000

    edit topology

--- exercise_2/extweetwordcount/topologies/tweetwordcount.clj
+++ exercise_2/extweetwordcount/topologies/tweetwordcount.clj
@@ -18,14 +18,14 @@
           {"tweet-spout" :shuffle}
           "bolts.parse.ParseTweet"
           ["word"]
-          :p 2
+          :p 1
           )
      "count-bolt" (python-bolt-spec
           options
           {"parse-tweet-bolt" ["word"]}
           "bolts.wordcount.WordCounter"
           ["word" "count"]
-          :p 2
+          :p 1
           )
     }
   ]
